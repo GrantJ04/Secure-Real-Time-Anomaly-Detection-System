e7eb71010946db93464ef34718ab64f989837023
GrantJ04 <grantjohnson0408@gmail.com>
2025-07-20 19:23:33 -0500

    Model Implemented

--- kaggle.py
+++ kaggle.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
-import sklearn
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 import plotly
 import flask
 import matplotlib.pyplot as plt
@@ -25,5 +29,30 @@ def datastream(): #loads datastream
         print(f"Amount: ${amt:<10} | Class: {cls:<5} | Delay {delay:<6.2f}")
         time.sleep(delay)
 
+def trainSave():
+    data = pd.read_csv("creditcard.csv")
+    x = data.drop('Class', axis = 1) #all cols except class
+    y = data['Class'] #class col
+
+    #split into training and testing sets
+
+    #80train/20test
+    xtrain, xtest, ytrain, ytest = train_test_split(x,y, test_size = 0.2, random_state = 42)
+
+    scaler = StandardScaler() #creates a scalar
+
+    xtrainScaled = scaler.fit_transform(xtrain)
+    xtestScaled = scaler.transform(xtest)
+
+    model = RandomForestClassifier(n_estimators = 100, random_state = 42)
+    model.fit(xtrainScaled, ytrain)
+
+    yPred = model.predict(xtestScaled)
+    print(classification_report(ytest, yPred))
+    joblib.dump(model, 'fraud_model.pkl')
+    joblib.dump(scaler, 'scaler.pkl')
+    print("Model and scaler saved!")
+
 if __name__ == "__main__":
-    datastream()
+    #datastream()
+    trainSave()
